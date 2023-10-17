938d04e3203391847c3e593d13ce79a8c635b918
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2023-10-17 17:11:14 +0200

    Dynamic Programming

--- src/lesson20231710/Fibonacci.java
+++ src/lesson20231710/Fibonacci.java
@@ -3,10 +3,10 @@ package lesson20231710;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        // 2 6 18 54  * 3 геом. прогрессия
-        // 2 5 8 11 14 + 3 арифм. прогрессия
+        // 2 5 8 11 14 // + 3 арифмемическая прогрессия
+        // 2 6 18 54  // * 3 геометрическая прогрессия
 
-        // 0 1 1 2 3 5 8 13 21
+        // 0 1 1 2 3 5 8 13 21 // последовательность Фибоначчи
         // fibonacci(0) = 0
         // fibonacci(1) = 1
         // fibonacci(2) = 1
@@ -16,15 +16,22 @@ public class Fibonacci {
         // fibonacci(6) = 8
 
         long start = System.nanoTime();
-        int result = fibonacciRecursiveMemo(45);
+        int result = fibonacciRecursive(45);
         long end = System.nanoTime();
-        System.out.println(result);
-        System.out.println("Elapsed time RecursiveMemo alg: " + (end - start));
+        System.out.println("Result = " + result);
+        System.out.println("Elapsed time, recursive alg: " + (end - start));
+
         start = System.nanoTime();
-        result = fibonacciRecursive(45);
+        result = fibonacciRecursiveMemo(45);
         end = System.nanoTime();
-        System.out.println(result);
-        System.out.println("Elapsed time Recursive alg: " + (end - start));
+        System.out.println("Result = " + result);
+        System.out.println("Elapsed time, recursiveMemo alg: " + (end - start));
+
+        start = System.nanoTime();
+        result = fibonacciTabulation(45);
+        end = System.nanoTime();
+        System.out.println("Result = " + result);
+        System.out.println("Elapsed time tabulation alg: " + (end - start));
     }
 
     public static int fibonacciRecursive(int n) {
@@ -39,15 +46,30 @@ public class Fibonacci {
         int[] arr = new int[n + 1];
         arr[0] = 0;
         arr[1] = 1;
-        return fibonacciRecursiveMemo(n - 2, arr) + fibonacciRecursiveMemo(n - 1, arr);
+        return fibonacciRecursiveMemo(n, arr);
     }
 
     private static int fibonacciRecursiveMemo(int n, int[] arr) {
         if (n == 0) return 0; // base condition
         if (n == 1) return 1; // base condition
         if (arr[n] != 0) return arr[n]; // cache
-        arr[n] = fibonacciRecursiveMemo(n - 2, arr) + fibonacciRecursiveMemo(n - 1, arr);
+        arr[n] = fibonacciRecursiveMemo(n - 1, arr) + fibonacciRecursiveMemo(n - 2, arr);
         return arr[n];
+    }
+
+    public static int fibonacciTabulation(int n){
+        if (n == 0) return 0; // base condition
+        if (n == 1) return 1; // base condition
+
+        int cache1 = 0;
+        int cache2 = 1;
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = cache1 + cache2;
+            cache1 = cache2;
+            cache2 = result;
+        }
+        return result;
     }
 
 
---
+++ src/lesson20231710/LadderBall.java
@@ -0,0 +1,44 @@
+package lesson20231710;
+
+public class LadderBall {
+
+    /*
+    На вершине лесенки, содержащей N ступенек, находится мячик, который начинает прыгать по ним вниз, к основанию.
+    Мячик может прыгнуть на следующую ступеньку, на ступеньку через одну или через 2.
+    (То есть, если мячик лежит на 8-ой ступеньке, то он может переместиться на 5-ую, 6-ую или 7-ую.)
+    Определить число всевозможных «маршрутов» мячика с вершины на землю.
+     */
+    public static void main(String[] args) {
+        System.out.println(ladderBall(5));
+        System.out.println(ladderBallTabulation(5));
+    }
+
+    public static int ladderBall(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 4;
+        // n = 4: 1 + 2 + 4 = 7
+        // n = 5: 2 + 4 + 7 = 13
+        return ladderBall(n - 1) + ladderBall(n - 2) + ladderBall(n - 3);
+    }
+
+    public static int ladderBallTabulation(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 4;
+
+        int cache1 = 1;
+        int cache2 = 2;
+        int cache3 = 4;
+        int result = 4;
+
+        for (int i = 4; i <= n; i++) {
+            result = cache1 + cache2 + cache3;
+            cache1 = cache2;
+            cache2 = cache3;
+            cache3 = result;
+        }
+        return result;
+    }
+
+}
